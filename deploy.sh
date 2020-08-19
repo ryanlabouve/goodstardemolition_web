7239ddc40bd7b532155d15588193895c73a78655
Ryan LaBouve <imryanlabouve@gmail.com>
2020-08-18 22:23:59 -0500

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@ git co -b gh-pages
 JEKYLL_ENV=production bundle exec jekyll build
 rm -rf $(ls | sort -u | grep -Ev '_site|deploy.sh')
 mv _site/* .
+git add .
+git commit -m "Deploy"
 git push origin gh-pages -f
 git co master
